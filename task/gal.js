8231d7cf5e3ab7c8cce6f7c6b439c79857f708a2
piyush6881 <piyush.r.tripathi@gmail.com>
2024-08-06 08:27:17 +0530

    Lecture Data

--- task/gal.js
+++ task/gal.js
@@ -1,30 +1,40 @@
 $(document).ready(function () {
 
     $(".btn").click(function () {
-        $(".gal1").css({
+        $(".gal1").animate({
             top: "0",
             left: "0",
-        });
+        },
+            2000
+        );
 
-        $(".gal2").css({
+        $(".gal2").animate({
             top: "160px",
             left: "160px",
-        });
+        },
+        2000
+    );
 
-        $(".gal3").css({
+        $(".gal3").animate({
             top: "0",
             left: "320px",
-        });
-        
-        $(".gal4").css({
+        }
+        ,
+        2000);
+
+        $(".gal4").animate({
             top: "160px",
             left: "480px",
-        });
+        },
+        2000);
 
-        $(".gal5").css({
+        $(".gal5").animate({
             top: "0",
             left: "640px",
-        });
+        },
+        2000);
+
+
 
     });
 
