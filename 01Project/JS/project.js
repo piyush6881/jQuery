d091bca01eafd9407a852379ab56d822510fdefd
piyush6881 <piyush.r.tripathi@gmail.com>
2024-08-23 12:45:58 +0530

    Lecture Data

--- 01Project/JS/project.js
+++ 01Project/JS/project.js
@@ -102,5 +102,13 @@ $('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
 $('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
 $('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
 
-
+// Calander
+$('#calendar').datepicker({
+    inline:true,
+    firstDay: 1,
+    showOtherMonths:true,
+    dayNamesMin:['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  });
+  
+  
 });
